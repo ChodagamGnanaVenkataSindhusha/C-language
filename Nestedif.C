41ae262c2c364e6a97ad5d84f47962e5502abcf0
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2024-10-25 14:06:17 +0530

    Create Nestedif.C

---
+++ Nestedif.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter the values of a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("a is larger value");
+        }
+    }
+   else if(b>c)
+    {
+        printf("b is larger value");
+    }
+    else
+    { 
+        printf(" c is larger value");
+    }
+}
+    
+    
+    
