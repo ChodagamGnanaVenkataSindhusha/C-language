72353481e858b7935117f5f2b52870a0e79b7574
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2024-10-25 11:56:05 +0530

    Create Ifelse1.C

---
+++ Ifelse1.C
@@ -0,0 +1,15 @@
+# include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n value:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("the given is number is positive");
+    }
+    else
+    {
+        printf(" the given number is negative");
+    }
+}
