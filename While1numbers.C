e593f2a9ac420d0a1fa50d0dd346ff98770fad4a
ChodagamGnanaVenkataSindhusha <sindhushachodagam689@gmail.com>
2024-12-20 18:50:39 +0530

    Create While1numbers.C

---
+++ While1numbers.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i=0,j=10;
+printf("the numbers from 1 to 10:");
+while(i<=10) 
+{
+printf("%d\n",i);
+i++;
+}
+printf("the numbers from 10 to 0:");
+while(j>=0)
+{
+    printf("%d\n",j);
+    j--;
+}
+}
